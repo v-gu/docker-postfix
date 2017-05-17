0d9822d1d1cceb60ae4157c37bcb716198934255
Vincent Gu <g@v-io.co>
2017-05-17 13:05:15 +0800

    fix scripts

--- scripts/postfix/main.sh
+++ scripts/postfix/main.sh
@@ -1,20 +1,38 @@
 #!/usr/bin/env sh
 set -e
 
-postfix_confdir=/etc/postfix
-
 # add main.cf entries
-echo "$(awk -v var=$POSTFIX_HOSTNAME '/^myhostname =/{f=1}END{ if (!f) {print "myhostname = "var}}1' ${postfix_confdir}/main.cf)" > ${postfix_confdir}/main.cf
-echo "$(awk -v var=$POSTFIX_DOMAIN '/^mydomain =/{f=1}END{ if (!f) {print "mydomain = "var}}1' ${postfix_confdir}/main.cf)" > ${postfix_confdir}/main.cf
-echo "$(awk -v var=$POSTFIX_ORIGIN '/^myorigin =/{f=1}END{ if (!f) {print "myorigin = "var}}1' ${postfix_confdir}/main.cf)" > ${postfix_confdir}/main.cf
+main="$(< ${APP_DIR}/main.cf)"
+echo "${main}" | \
+    awk \
+        -v POSTFIX_HOSTNAME=$POSTFIX_HOSTNAME \
+        -v POSTFIX_DOMAIN=$POSTFIX_DOMAIN \
+        -v POSTFIX_ORIGIN=$POSTFIX_ORIGIN \
+        -v POSTFIX_VA_DOMAINS=$POSTFIX_VA_DOMAINS \
+        -v POSTFIX_VA_MAPS=$POSTFIX_VA_MAPS \
+        -v POSTFIX_TRANSPORTS=$POSTFIX_TRANSPORTS \
+        ' \
+        /^myhostname[[:blank:]]*=/{myhostname=1; if (myhostname) {sub(/=.*$/, "= "POSTFIX_HOSTNAME)}} \
+        /^mydomain[[:blank:]]*=/{mydomain=1; if (mydomain) {sub(/=.*$/, "= "POSTFIX_DOMAIN)}} \
+        /^myorigin[[:blank:]]*=/{myorigin=1; if (myorigin) {sub(/=.*$/, "= "POSTFIX_ORIGIN)}} \
+        /^virtual_alias_domains[[:blank:]]*=/{virtual_alias_domains=1; if (virtual_alias_domains) {sub(/=.*$/, "= "POSTFIX_VA_DOMAINS)}} \
+        /^virtual_alias_maps[[:blank:]]*=/{virtual_alias_maps=1; if (virtual_alias_maps) {sub(/=.*$/, "= "POSTFIX_VA_MAPS)}} \
+        /^transport_maps[[:blank:]]*=/{transport_maps=1; if (transport_maps) {sub(/=.*$/, "= "POSTFIX_TRANSPORTS)}} \
+        {print $0}
+        END {print ""} \
+        END {if (!myhostname) {print "myhostname = "POSTFIX_HOSTNAME}} \
+        END {if (!mydomain) {print "mydomain = "POSTFIX_DOMAIN}} \
+        END {if (!myorigin) {print "myorigin = "POSTFIX_ORIGIN}} \
+        END {if (!virtual_alias_domains) {print "virtual_alias_domains = "POSTFIX_VA_DOMAINS}} \
+        END {if (!virtual_alias_maps) {print "virtual_alias_maps = "POSTFIX_VA_MAPS}} \
+        END {if (!transport_maps) {print "transport_maps = "POSTFIX_TRANSPORTS}} \
+        ' \
+        > "${APP_DIR}/main.cf"
 
 # add virtual entries
-echo "$(awk -v var=$POSTFIX_VA_DOMAIN '/^virtual_alias_domains =/{f=1}END{ if (!f) {print "virtual_alias_domains = "var}}1' ${postfix_confdir}/main.cf)" > ${postfix_confdir}/main.cf
-echo "$(awk '/^virtual_alias_maps =/{f=1}END{ if (!f) {print "virtual_alias_maps = hash:/etc/postfix/virtual"}}1' ${postfix_confdir}/main.cf)" > ${postfix_confdir}/main.cf
-echo -e "$POSTFIX_VA_ENTRIES" > ${postfix_confdir}/virtual
-postmap ${postfix_confdir}/virtual
+echo -e "$POSTFIX_VA_MAPS" > ${APP_DIR}/virtual
+postmap ${APP_DIR}/virtual
 
 # add transport entries
-echo "$(awk '/^transport_maps =/{f=1}END{ if (!f) {print "transport_maps = hash:/etc/postfix/transport"}}1' ${postfix_confdir}/main.cf)" > ${postfix_confdir}/main.cf
-echo -e "$POSTFIX_TRANSPORTS" > ${postfix_confdir}/transport
-postmap ${postfix_confdir}/transport
+echo -e "$POSTFIX_TRANSPORTS" > ${APP_DIR}/transport
+postmap ${APP_DIR}/transport
