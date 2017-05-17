5375e33b3c3380c0b86ce34c36ef6628c0c28484
Vincent Gu <g@v-io.co>
2017-05-17 18:09:39 +0800

    fix virtual_alias_maps and transport_maps

--- scripts/postfix/main.sh
+++ scripts/postfix/main.sh
@@ -39,6 +39,7 @@ echo "${master}" | \
 main="$(< ${APP_DIR}/main.cf)"
 echo "${main}" | \
     gawk \
+        -v APP_DIR="${APP_DIR}" \
         -v POSTFIX_HOSTNAME="$POSTFIX_HOSTNAME" \
         -v POSTFIX_DOMAIN="$POSTFIX_DOMAIN" \
         -v POSTFIX_ORIGIN="$POSTFIX_ORIGIN" \
@@ -49,17 +50,17 @@ echo "${main}" | \
         /^myhostname[[:blank:]]*=/{myhostname=1; if (myhostname) {sub(/=.*$/, "= "POSTFIX_HOSTNAME)}} \
         /^mydomain[[:blank:]]*=/{mydomain=1; if (mydomain) {sub(/=.*$/, "= "POSTFIX_DOMAIN)}} \
         /^myorigin[[:blank:]]*=/{myorigin=1; if (myorigin) {sub(/=.*$/, "= "POSTFIX_ORIGIN)}} \
-        /^virtual_alias_domains[[:blank:]]*=/{virtual_alias_domains=1; if (virtual_alias_domains) {sub(/=.*$/, "= "POSTFIX_VA_DOMAINS)}} \
-        /^virtual_alias_maps[[:blank:]]*=/{virtual_alias_maps=1; if (virtual_alias_maps) {sub(/=.*$/, "= "POSTFIX_VA_MAPS)}} \
-        /^transport_maps[[:blank:]]*=/{transport_maps=1; if (transport_maps) {sub(/=.*$/, "= "POSTFIX_TRANSPORTS)}} \
+        /^virtual_alias_domains[[:blank:]]*=/{virtual_alias_domains=1; if (virtual_alias_domains) {sub(/=.*$/, "= " POSTFIX_VA_DOMAINS)}} \
+        /^virtual_alias_maps[[:blank:]]*=/{virtual_alias_maps=1; if (virtual_alias_maps) {sub(/=.*$/, "= hash:" APP_DIR "/virtual")}} \
+        /^transport_maps[[:blank:]]*=/{transport_maps=1; if (transport_maps) {sub(/=.*$/, "= hash:" APP_DIR "/transport")}} \
         {print $0}
         END {print ""} \
         END {if (!myhostname) {print "myhostname = "POSTFIX_HOSTNAME}} \
         END {if (!mydomain) {print "mydomain = "POSTFIX_DOMAIN}} \
         END {if (!myorigin) {print "myorigin = "POSTFIX_ORIGIN}} \
-        END {if (!virtual_alias_domains) {print "virtual_alias_domains = "POSTFIX_VA_DOMAINS}} \
-        END {if (!virtual_alias_maps) {print "virtual_alias_maps = "POSTFIX_VA_MAPS}} \
-        END {if (!transport_maps) {print "transport_maps = "POSTFIX_TRANSPORTS}} \
+        END {if (!virtual_alias_domains) {print "virtual_alias_domains = " POSTFIX_VA_DOMAINS}} \
+        END {if (!virtual_alias_maps) {print "virtual_alias_maps = hash:" APP_DIR "/virtual"}} \
+        END {if (!transport_maps) {print "transport_maps = hash:" APP_DIR "/transport"}} \
         ' \
         > "${APP_DIR}/main.cf"
 
