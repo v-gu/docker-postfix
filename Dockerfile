d94c2c5456f17b974676b86d8655eb6fb95f6a6a
Vincent Gu <g@v-io.co>
2017-08-01 18:44:53 +0800

    add opendkim function

--- Dockerfile
+++ Dockerfile
@@ -19,13 +19,19 @@ ENV SASL2_DIR                           "${ROOT_DIR}/sasl2"
 ENV POSTFIX_HOSTNAME                    ""
 ENV POSTFIX_DOMAIN                      "${POSTFIX_HOSTNAME}"
 ENV POSTFIX_ORIGIN                      "${POSTFIX_DOMAIN}"
-ENV POSTFIX_SMTP_PORT                   smtp
+ENV POSTFIX_SMTP_PORT                   25
 
 ENV USE_SUBMISSION                      no
-ENV POSTFIX_SUBM_PORT                   submission
+ENV POSTFIX_SUBM_PORT                   587
 ENV POSTFIX_SMTP_TLS_CERT_FILE          ""
 ENV POSTFIX_SMTP_TLS_KEY_FILE           ""
 ENV SASLDB_PATH                         "/etc/sasldb2"
+ENV DKIM_LISTEN_ADDR                    "127.0.0.1"
+ENV DKIM_LISTEN_PORT                    "8891"
+ENV DKIM_DOMAIN                         "${POSTFIX_DOMAIN}"
+ENV DKIM_SELECTOR                       "mail"
+ENV DKIM_KEY_FILE                       "/etc/opendkim.d/${DKIM_SELECTOR}.private"
+ENV DKIM_TRUSTED_HOSTS                  "127.0.0.1\n::1\nlocalhost\n\n\*.example.com"
 
 ENV USE_POSTSRSD                        no
 ENV POSTFIX_VA_DOMAINS                  "${POSTFIX_DOMAIN}"
@@ -37,7 +43,7 @@ ENV SRS_FORWARD_PORT                    10001
 ENV SRS_REVERSE_PORT                    10002
 ENV SRS_SEPARATOR                       "="
 ENV SRS_TIMEOUT                         1800
-ENV SRS_SECRET                          "${POSTSRSD_DIR}/postsrsd.secret}"
+ENV SRS_SECRET                          "${POSTSRSD_DIR}/postsrsd.secret"
 ENV SRS_PID_FILE                        ""
 ENV SRS_RUN_AS                          ""
 ENV SRS_CHROOT                          ""
--- imagescripts/run.sh
+++ imagescripts/run.sh
@@ -1,5 +1,46 @@
 #!/usr/bin/env bash
 
+# init vars
+POSTFIX_DIR="${POSTFIX_DIR}"
+POSTSRSD_DIR="${POSTSRSD_DIR}"
+OPENDKIM_DIR="${OPENDKIM_DIR}"
+SASL2_DIR="${SASL2_DIR}"
+
+POSTFIX_HOSTNAME="${POSTFIX_HOSTNAME}"
+POSTFIX_DOMAIN="${POSTFIX_DOMAIN:-${POSTFIX_HOSTNAME}}"
+POSTFIX_ORIGIN="${POSTFIX_ORIGIN:-${POSTFIX_HOSTNAME}}"
+POSTFIX_SMTP_PORT="${POSTFIX_SMTP_PORT:-smtp}"
+
+USE_SUBMISSION="${USE_SUBMISSION:-no}"
+POSTFIX_SUBM_PORT="${POSTFIX_SUBM_PORT:-submission}"
+POSTFIX_SMTP_TLS_CERT_FILE="${POSTFIX_SMTP_TLS_CERT_FILE}"
+POSTFIX_SMTP_TLS_KEY_FILE="${POSTFIX_SMTP_TLS_KEY_FILE}"
+SASLDB_PATH="${SASLDB_PATH:-/etc/sasldb2}"
+DKIM_LISTEN_ADDR="${DKIM_LISTEN_ADDR:-127.0.0.1}"
+DKIM_LISTEN_PORT="${DKIM_LISTEN_PORT:-9901}"
+DKIM_DOMAIN="${DKIM_DOMAIN:-${POSTFIX_DOMAIN}}"
+DKIM_SELECTOR="${DKIM_SELECTOR:-mail}"
+DKIM_KEY_FILE="${DKIM_KEY_FILE:-/etc/opendkim.d/${DKIM_SELECTOR}.private}"
+DKIM_TRUSTED_HOSTS="${DKIM_TRUSTED_HOSTS:-127.0.0.1\n::1\nlocalhost\n\n\*.example.com}"
+
+USE_POSTSRSD="${USE_POSTSRSD:-no}"
+POSTFIX_VA_DOMAINS="${POSTFIX_VA_DOMAINS:-${POSTFIX_DOMAIN}}"
+POSTFIX_VA_MAPS="${POSTFIX_VA_MAPS}"
+POSTFIX_TRANSPORTS="${POSTFIX_TRANSPORTS}"
+SRS_LISTEN_ADDR="${SRS_LISTEN_ADDR:-127.0.0.1}"
+SRS_DOMAIN="${SRS_DOMAIN:-${POSTFIX_DOMAIN}}"
+SRS_FORWARD_PORT="${SRS_FORWARD_PORT:-10001}"
+SRS_REVERSE_PORT="${SRS_REVERSE_PORT:-10002}"
+SRS_SEPARATOR="${SRS_SEPARATOR:-=}"
+SRS_TIMEOUT="${SRS_TIMEOUT:-1800}"
+SRS_SECRET="${SRS_SECRET:-${POSTSRSD_DIR}/postsrsd.secret}"
+SRS_PID_FILE="${SRS_PID_FILE}"
+SRS_RUN_AS="${SRS_RUN_AS}"
+SRS_CHROOT="${SRS_CHROOT}"
+SRS_EXCLUDE_DOMAINS="${SRS_EXCLUDE_DOMAINS}"
+SRS_REWRITE_HASH_LEN="${SRS_REWRITE_HASH_LEN:-4}"
+SRS_VALIDATE_HASH_MINLEN="${SRS_VALIDATE_HASH_MINLEN:-4}"
+
 # preparing app directories
 ln -sn /etc/postfix ${POSTFIX_DIR}
 ln -sn /etc/postsrsd ${POSTSRSD_DIR}
@@ -52,6 +93,13 @@ myorigin = ${POSTFIX_ORIGIN}
 virtual_alias_domains = ${POSTFIX_VA_DOMAINS}
 virtual_alias_maps = hash:${POSTFIX_DIR}/virtual
 transport_maps = hash:${POSTFIX_DIR}/transport
+
+# Milter settings.
+milter_protocol = 2
+milter_default_action = accept
+# OpenDKIM runs on port ${DKIM_LISTEN_ADDR}:${DKIM_LISTEN_PORT}.
+smtpd_milters = inet:${DKIM_LISTEN_ADDR}:${DKIM_LISTEN_PORT}
+non_smtpd_milters = inet:${DKIM_LISTEN_ADDR}:${DKIM_LISTEN_PORT}
 EOF
 
 # add submission configs if required
@@ -83,6 +131,43 @@ mech_list: PLAIN LOGIN CRAM-MD5 DIGEST-MD5 NTLM
 log_level: 7
 EOF
     ln -s ${SASL2_DIR}/smtpd.conf /usr/lib/sasl2/smtpd.conf
+
+    # add opendkim config
+    cat <<EOF > ${OPENDKIM_DIR}/opendkim.conf
+# OpenDKIM config.
+
+# Log to syslog
+BaseDirectory           ${OPENDKIM_DIR}
+
+Syslog                  yes
+SyslogSuccess           yes
+LogWhy                  yes
+# Required to use local socket with MTAs that access the socket as a non-
+# privileged user (e.g. Postfix)
+UMask                   002
+
+Mode                    sv
+PidFile                 ${OPENDKIM_DIR}/opendkim.pid
+UserID                  root:root
+Socket                  inet:${DKIM_LISTEN_PORT}@${DKIM_LISTEN_ADDR}
+
+Canonicalization        relaxed/simple
+SignatureAlgorithm      rsa-sha256
+
+# Sign for example.com with key in /etc/opendkim.d/mail.private using
+# selector 'mail' (e.g. mail._domainkey.example.com)
+Domain                  ${DKIM_DOMAIN}
+KeyFile                 ${DKIM_KEY_FILE}
+Selector                ${DKIM_SELECTOR}
+
+ExternalIgnoreList      refile:${OPENDKIM_DIR}/TrustedHosts
+InternalHosts           refile:${OPENDKIM_DIR}/TrustedHosts
+EOF
+
+    echo -e "${DKIM_TRUSTED_HOSTS}" > ${OPENDKIM_DIR}/TrustedHosts
+
+    # start opendkim server
+    opendkim
 fi
 
 #add SRS configs if required
@@ -107,45 +192,45 @@ EOF
 
     # run postsrsd
     cmd="postsrsd -D"
-    if [ -n "${SRS_LISTEN_ADDR+x}" ]; then
-        cmd+=" -l ${SRS_LISTEN_ADDR}"
-    fi
-    if [ -n "${SRS_DOMAIN+x}" ]; then
+    # if [ -n "${SRS_LISTEN_ADDR+x}" ]; then
+    #     cmd+=" -l ${SRS_LISTEN_ADDR}"
+    # fi
+    if [ -n "${SRS_DOMAIN}" ]; then
         cmd+=" -d ${SRS_DOMAIN}"
     fi
-    if [ -n "${SRS_SEPARATOR+x}" ]; then
+    if [ -n "${SRS_SEPARATOR}" ]; then
         cmd+=" -a ${SRS_SEPARATOR}"
     fi
-    if [ -n "${SRS_FORWARD_PORT+x}" ]; then
+    if [ -n "${SRS_FORWARD_PORT}" ]; then
         cmd+=" -f ${SRS_FORWARD_PORT}"
     fi
-    if [ -n "${SRS_REVERSE_PORT+x}" ]; then
+    if [ -n "${SRS_REVERSE_PORT}" ]; then
         cmd+=" -r ${SRS_REVERSE_PORT}"
     fi
-    if [ -n "${SRS_TIMEOUT+x}" ]; then
+    if [ -n "${SRS_TIMEOUT}" ]; then
         cmd+=" -t ${SRS_TIMEOUT}"
     fi
-    if [ -n "${SRS_SECRET+x}" ]; then
+    if [ -n "${SRS_SECRET}" ]; then
         cmd+=" -s ${SRS_SECRET}"
     fi
-    if [ -n "${SRS_PID_FILE+x}" ]; then
+    if [ -n "${SRS_PID_FILE}" ]; then
         cmd+=" -p ${SRS_PID_FILE}"
     fi
-    if [ -n "${SRS_RUN_AS+x}" ]; then
+    if [ -n "${SRS_RUN_AS}" ]; then
         cmd+=" -u ${SRS_RUN_AS}"
     fi
-    if [ -n "${SRS_CHROOT+x}" ]; then
+    if [ -n "${SRS_CHROOT}" ]; then
         cmd+=" -c ${SRS_CHROOT}"
     fi
-    if [ -n "${SRS_EXCLUDE_DOMAINS+x}" ]; then
+    if [ -n "${SRS_EXCLUDE_DOMAINS}" ]; then
         cmd+=" -X ${SRS_EXCLUDE_DOMAINS}"
     fi
-    if [ -n "${SRS_REWRITE_HASH_LEN+x}" ]; then
-        cmd+=" -n ${SRS_REWRITE_HASH_LEN}"
-    fi
-    if [ -n "${SRS_VALIDATE_HASH_MINLEN+x}" ]; then
-        cmd+=" -N ${SRS_VALIDATE_HASH_MINLEN}"
-    fi
+    # if [ -n "${SRS_REWRITE_HASH_LEN+x}" ]; then
+    #     cmd+=" -n ${SRS_REWRITE_HASH_LEN}"
+    # fi
+    # if [ -n "${SRS_VALIDATE_HASH_MINLEN+x}" ]; then
+    #     cmd+=" -N ${SRS_VALIDATE_HASH_MINLEN}"
+    # fi
     eval "${cmd}"
 fi
 
